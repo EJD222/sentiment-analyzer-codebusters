51b13a26692d66566bed705e8e28b7269eba0ec6
Paul Trustan Yumang <yumang.paultrustan@auf.edu.ph>
2024-11-25 18:17:30 +0800

    Add navigation

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
+
+Route::get('/sentiment', function () {
+    return view('sentiment');
+})->name('sentiment');
+
+Route::get('/about', function () {
+    return view('about');
+})->name('about');
+
+Route::get('/contact', function () {
+    return view('contact');
+})->name('contact');
