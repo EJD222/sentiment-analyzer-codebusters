7e8d2f542a7b160cbfa1efc2a274325a13380cd0
Paul Trustan Yumang <yumang.paultrustan@auf.edu.ph>
2024-11-25 19:16:32 +0800

    Add npl testing

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Web64\LaravelNlp\NlpClient;
+use Illuminate\Support\Facades\Http;
+
+Route::get('/test-sentiment', function () {
+    $text = "Laravel is amazing!";
+    $response = Http::asForm()->post('https://api.meaningcloud.com/sentiment-2.1', [
+        'key' => env('MEANINGCLOUD_API_KEY'),
+        'txt' => $text,
+        'lang' => 'en',
+    ]);
+    return $response->json();
+});
 
 Route::get('/', function () {
     return view('welcome');
