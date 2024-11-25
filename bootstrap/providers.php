8e1c04d8e0b94e064a1b04cf9b89941a3df6946c
Paul Trustan Yumang <yumang.paultrustan@auf.edu.ph>
2024-11-25 19:15:46 +0800

    Add provider

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,5 +1,14 @@
 <?php
 
 return [
+    Illuminate\Auth\AuthServiceProvider::class,
+    Illuminate\Broadcasting\BroadcastServiceProvider::class,
+    Illuminate\Bus\BusServiceProvider::class,
+    Illuminate\Cache\CacheServiceProvider::class,
+    Illuminate\Foundation\Providers\ConsoleSupportServiceProvider::class,
+    Illuminate\Queue\QueueServiceProvider::class,
+    Illuminate\Validation\ValidationServiceProvider::class,
+    Illuminate\View\ViewServiceProvider::class,
     App\Providers\AppServiceProvider::class,
+    Web64\LaravelNlp\NlpServiceProvider::class,
 ];
